fab76a2b873d80321d712a3bc88b3a10b07cb071
Mangust02 <accformy2@gmail.com>
2022-01-17 00:48:05 +0500

    Rashid

---
+++ bot/src/main/java/koinot/com/bot/entity/products/BusinessServices.java
@@ -0,0 +1,32 @@
+package koinot.com.bot.entity.products;
+
+import koinot.com.bot.entity.products.template.ReadyProduct;
+import koinot.com.bot.enums.TypeOfAgreement;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * @className: BotMessage  $
+ * @description: TODO
+ * @date: 09 January 2022 $
+ * @time: 6:35 AM $
+ * @author: Qudratjon Komilov
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="businessServices")
+public class BusinessServices extends ReadyProduct {
+
+    @Id @GeneratedValue(strategy=GenerationType.IDENTITY) private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private TypeOfAgreement typeOfAgreement;
+
+    private double price;
+
+}
