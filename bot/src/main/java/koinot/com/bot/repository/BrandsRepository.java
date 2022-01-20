0f8c049759b8eccae49ee12cf16fc1f3fc967a8d
Mangust02 <accformy2@gmail.com>
2022-01-20 12:21:51 +0500

    fixed repositories

---
+++ bot/src/main/java/koinot/com/bot/repository/BrandsRepository.java
@@ -0,0 +1,17 @@
+package koinot.com.bot.repository;
+
+
+import koinot.com.bot.entity.addons.Brands;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Komilov Qudrtajon
+ * @link Telegram Link https://t.me/qudratjon03031999
+ * @since 26/08/21
+ */
+
+@Repository
+public interface BrandsRepository extends JpaRepository<Brands, Long> {
+
+}
