f83b58c6fe1f7ff6155d58da9dae793499eee321
Mangust02 <accformy2@gmail.com>
2022-01-24 19:09:56 +0500

    fixed

---
+++ bot/src/main/java/koinot/com/bot/entity/products/Books.java
@@ -0,0 +1,29 @@
+package koinot.com.bot.entity.products;
+
+import koinot.com.bot.entity.products.template.ReadyProduct;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @className: BotMessage  $
+ * @description: TODO
+ * @date: 09 January 2022 $
+ * @time: 6:35 AM $
+ * @author: Qudratjon Komilov 
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="books")
+public class Books extends ReadyProduct {
+
+    @Id @GeneratedValue(strategy=GenerationType.IDENTITY) private Long id;
+
+}
